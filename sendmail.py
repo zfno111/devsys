93e2e88b83067a9fe378ee7b10acd57481de8f87
zfno111 <zfno111@163.com>
2019-08-20 08:35:38 +0800

    1

--- sendmail.py
+++ sendmail.py
@@ -6,7 +6,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'devsys.settings'
 if __name__ == '__main__':
 
     send_mail(
-        '来自www.liuji1angblog.com的测试邮件',
+        '来自www.liujiangblog.com的测试邮件',
         '欢迎访问www.liujiangblog.com，这里是刘江的博客和教程站点，本站专注于Python、Django和机器学习技术的分享！',
         'zfno111@sina.com',
         ['1367615159@qq.com'],
