fef75877ec7423b9ca56ab2cd8e98c07e3694e3a
zfno111 <zfno111@163.com>
2019-08-27 10:15:58 +0800

    1

--- assets/mongo.py
+++ assets/mongo.py
@@ -13,30 +13,63 @@ db = client.monitor
 db.authenticate("monitor", "DReqd0AqLCUTgVzx")
 # 连接表
 collection = db.myset
-results=list(collection.find())
-
-#获取所有的资产信息
-
-assets = Asset.objects.get(name="weather3-og.pagoda.com.cn")
-
-# 访问表的一行数据
-results=collection.find_one()
-print(results)
-results['asset_type']='server'
-results['sn']=results['InstanceId']
-results['os_distribution']='Linux'
-results['os_type']='Linux'
-results['os_release']=results['OsName']
-results['cpu_count']=1
-results['cpu_model']='inter'
-results['cpu_core_count']=results['CPU']
-#固定插槽
-results['ram']=[{"slot": "A1","capacity": results['Memory']}]
-# results['physical_disk_driver']=[{"model":"FAST","size":results['DataDisks'][0]['DiskSize'],"sn":results['DataDisks'][0]['DiskId']}]
-results['physical_disk_driver']=[{"model":"FAST","size":"666","sn":results['DataDisks'][0]['DiskId']}]
-results['idcname']=results['Placement']['Zone']
-results['ip']=results['PrivateIpAddresses']
-print(results['physical_disk_driver'])
-print(results['DataDisks'][0]['DiskSize'])
-#转换为json格式
-print(json_util.dumps(results))
+# results=list(collection.find())
+results=list(collection.find().limit(900))
+for i in results:
+    name1=i['InstanceId']
+    print(i)
+    #环境名
+    if "uat" in name1:
+        envir1="uat环境"
+    elif "test" in name1:
+        envir1="测试环境"
+    elif "dev" in "name1":
+        envir1="dev环境"
+    else:
+        envir1="生产环境"
+    #print(envir1)
+    #操作系统
+    ossys1=i['OsName']
+    print(ossys1)
+    #用户名
+    userid1="root"
+    #密码
+    userpasswd1=""
+    #内网IP
+    ipin1=i['PrivateIpAddresses'][0]
+    print(ipin1)
+    #外网IP
+    if i['PublicIpAddresses']:
+        ipout1=i['PublicIpAddresses']
+    else:
+        ipout1=""
+    print("ipout")
+    #CPU信息
+    cpu1=i['CPU']
+    #内存信息
+    mem1=i['Memory']
+    #描述信息
+    desc1=""
+    #系统盘
+    sysdisk1=i['SystemDisk']['DiskSize']
+    print(sysdisk1)
+    #数据盘
+    if i['DataDisks']:
+        disksize=0
+        for p in i['DataDisks']:
+            disksize+=p['DiskSize']
+        print("总大小为%d"%(disksize))
+    datadisk1=disksize
+    #带宽
+    bandwidth1=i['InternetAccessible']['InternetMaxBandwidthOut']
+    print(bandwidth1)
+    #secgroup
+    if i['SecurityGroupIds']:
+        secgroup1=i['SecurityGroupIds'][0]
+    print(secgroup1)
+    #分组
+    if i ['Tags']:
+        group1=i['Tags'][0]['Value']
+    else:
+        group1=""
+    print(group1)
